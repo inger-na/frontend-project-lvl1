e10035b20c75343c7e208485b2135fab88598f8e
inger_na <inger_na@mail.ru>
2020-04-09 14:43:31 +0200

    Add eslint, make lint and fix eslint config

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  presets: [
+    ['@babel/env', {
+      targets: {
+        node: 'current',
+        firefox: '60',
+        chrome: '67',
+        safari: '11.1',
+      },
+    }],
+  ],
+  plugins: [
+    '@babel-plugin-dynamic-import-node',
+    '@babel/plugin-proposal-export-default-from',
+  ],
+};
