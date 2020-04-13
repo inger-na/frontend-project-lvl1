7509598b4322291b93c2ca81c7ee5c4f10dafb9c
inger_na <inger_na@mail.ru>
2020-04-13 17:39:05 +0200

    Commit

--- babel.config.js
+++ babel.config.js
@@ -5,12 +5,12 @@ module.exports = {
         node: 'current',
         firefox: '60',
         chrome: '67',
-        safari: '11.1',
-      },
-    }],
+        safari: '11.1'
+      }
+    }]
   ],
   plugins: [
     '@babel-plugin-dynamic-import-node',
-    '@babel/plugin-proposal-export-default-from',
-  ],
+    '@babel/plugin-proposal-export-default-from'
+  ]
 };
